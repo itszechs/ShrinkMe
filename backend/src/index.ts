d2857cdf79d7755bcf82e76f6aef74e5045e60d2
zechs <vijay.vky2002@gmail.com>
2023-01-22 01:24:47 +0530

    added links router

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,41 @@
+import dotenv from "dotenv";
+
 import express, { Request, Response, NextFunction } from "express";
+import { connectToDatabase } from "./services/database";
+import { linksRouter } from "./routes/links";
 
-const app = express();
+// Load environment variables from the .env file, where the ATLAS_URI is configured
+dotenv.config();
 
-app.listen(5500, () => {
-    console.log(`Server running at http://localhost:5500`);
-});
+const { ATLAS_URI } = process.env;
 
-// Health check route
-app.get("/", (_req: Request, res: Response) => {
-    res.send({
-        message: "ShrinkMe api is running!",
-    });
-});
+if (!ATLAS_URI) {
+    console.error("No ATLAS_URI environment variable has been defined in config.env");
+}
 
+connectToDatabase(ATLAS_URI)
+    .then(() => {
+        const app = express();
 
-// Invalid route message
-app.use((_req: Request,  res: Response, _next : NextFunction) => {
-    res.status(404).send({
-        error: "Not Found",
-    });
-});
+        app.listen(5500, () => {
+            console.log(`Server running at http://localhost:5500`);
+        });
+
+        // Health check route
+        app.get("/", (_req: Request, res: Response) => {
+            res.send({
+                message: "ShrinkMe api is running!",
+            });
+        });
+
+        app.use("/api/v1/links", linksRouter);
+
+        // Invalid route message
+        app.use((_req: Request, res: Response, _next: NextFunction) => {
+            res.status(404).send({
+                error: "Not Found",
+            });
+        });
+
+    })
+    .catch(error => console.error(error));
