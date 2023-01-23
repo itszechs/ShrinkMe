b8fccbfeaa81df20f28e2a5b65d1440a8090a332
Jed <prernakumari480@gmail.com>
2023-01-23 19:28:05 +0530

    implemented error page layout

--- frontend/src/components/ErrorPage.tsx
+++ frontend/src/components/ErrorPage.tsx
@@ -3,9 +3,9 @@ import './ErrorPage.css'
 export default function ErrorPage() {
   return (
     <div className="error-page">
-        <span>404</span>
-        <span>Not Found</span>
-        <span>The resource requested could not be found on this server</span>
+        <span className='header'>404</span>
+        <span className='content'>Not Found</span>
+        <span className='description'>The resource requested could not be found on this server</span>
     </div>
   );
 }
