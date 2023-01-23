e2d56dab0415e8facd30f6cb0a837cc7bd2526e9
Jed <prernakumari480@gmail.com>
2023-01-23 19:16:27 +0530

    error page description

--- frontend/src/components/ErrorPage.tsx
+++ frontend/src/components/ErrorPage.tsx
@@ -3,7 +3,9 @@ import './ErrorPage.css'
 export default function ErrorPage() {
   return (
     <div className="error-page">
-        404
+        <span>404</span>
+        <span>Not Found</span>
+        <span>The resource requested could not be found on this server</span>
     </div>
   );
 }
