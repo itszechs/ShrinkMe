600fa0327c240f2c5091b430fe2fbf609fefbb84
zechs <vijay.vky2002@gmail.com>
2023-04-13 02:22:27 +0530

    handle redirection if logged in

--- frontend/src/containers/App.tsx
+++ frontend/src/containers/App.tsx
@@ -1,10 +1,12 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from '../components/HomePage';
 import ErrorPage from '../components/ErrorPage';
 import NavBar from '../components/NavBar';
 import AuthPage from '../components/AuthPage';
 
 export default function App() {
+  const token = localStorage.getItem("token");
+
   return (
     <BrowserRouter >
       <Routes>
@@ -13,7 +15,7 @@ export default function App() {
             <NavBar />
             <HomePage /></>
         } />
-        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/auth" element={token ? <Navigate to="/" replace /> : <AuthPage />} />
         <Route path="/not-found" element={<ErrorPage />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
