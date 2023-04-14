5de358b9be75e721e1c58a8ba1933e7978a35432
zechs <vijay.vky2002@gmail.com>
2023-04-14 10:47:20 +0530

    implemented dashboard page

--- frontend/src/components/DashPage.tsx
+++ frontend/src/components/DashPage.tsx
@@ -1,17 +1,77 @@
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
 import { AuthContext } from "../containers/App"
+import { Fade, Spinner } from "reactstrap";
+import { api } from "../utils/constants";
+import LinkCard from "./LinkCard";
+import "./DashPage.css"
+
+interface UserLink {
+    id: string,
+    shortenUrl: string,
+    originalUrl: string,
+}
 
 export default function DashPage() {
     const { isLoggedIn } = useContext(AuthContext);
+    const [links, setLinks] = useState<UserLink[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
 
     useEffect(() => {
         if (!isLoggedIn) {
             window.location.href = "/";
         }
-    }, [isLoggedIn])
+
+        setLoading(true);
+
+        fetch(api.buildUrl(api.links), {
+            method: "GET",
+            headers: {
+                "Content-type": "application/json",
+                "Authorization": `Bearer ${localStorage.getItem("token")}`
+            }
+        }).then(res => {
+            if (res.ok) {
+                return res.json();
+            }
+            return Promise.reject(res);
+        }).then((response) => {
+            let _links: UserLink[] = [];
+            for (let i = 0; i < response.length; i++) {
+                _links.push(response[i]);
+            }
+            setLinks(_links);
+            setLoading(false);
+        }).catch(err => {
+            console.log(err);
+            err.json().then((error: any) => {
+                console.log(error.message);
+                // setOutput(error.message);
+            });
+            setLoading(false);
+        });
+
+    }, [isLoggedIn]);
 
     return (
         <div className="dash-page">
+            <div className="heading dash-heading">
+                <span className="tagline">Your Dashboard: Manage Your Shortened URLs</span>
+                <span className="tagline sub">Edit or delete your links anytime from your dashboard</span>
+            </div>
+            {loading ? (
+                <Spinner style={{ marginTop: "20px" }} />
+            ) : (
+                <Fade tag="div">
+                    <div className="links-grid">
+                        {links.map((link, index) => (
+                            <LinkCard key={index}
+                                title={link.originalUrl}
+                                subtitle={link.shortenUrl}
+                            />
+                        ))}
+                    </div>
+                </Fade>
+            )}
         </div>
     )
 }
---
+++ frontend/src/components/LinkCard.tsx
@@ -0,0 +1,40 @@
+import { Card, CardText, CardBody, Button } from 'reactstrap';
+import './LinkCard.css';
+
+interface Props {
+    title: string;
+    subtitle: string;
+}
+
+export default function LinkCard(props: Props) {
+    return (
+        <Card className="link-card">
+            <CardBody>
+                <div className="d-flex align-items-center">
+                    <div className="text-truncate">
+                        <CardText tag="h5" className="text-truncate">{props.title}</CardText>
+                        <CardText ctag="h6" className="mb-2 text-truncate shorten-link">
+                            {props.subtitle}
+                            <i className="bi bi-box-arrow-up-right" style={{ marginLeft: "8px" }}></i>
+                        </CardText>
+
+                    </div>
+                    <div className="ms-auto"
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "flex-end",
+                            justifyContent: "center",
+                        }}>
+                        <Button color="success" className="btn-icon btn-green" >
+                            <i className="bi bi-pencil-fill"></i>
+                        </Button>
+                        <Button color="danger" className="btn-icon btn-red mt-1">
+                            <i className="bi bi-trash-fill"></i>
+                        </Button>
+                    </div>
+                </div>
+            </CardBody>
+        </Card>
+    );
+};
